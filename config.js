e11993138cc73fa2306373eeaaa54f5ff4fb5e2f
Danscot <scotfielddaniel@gmail.com>
2025-08-30 17:24:56 +0100

    Last minute fix

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 
 
-export const TELEGRAM_BOT_TOKEN = '8058649447:AAGOAYALpfaPmIenSQm1aS9z-4-_tDpYZgE'; // bot telegram token
+export const TELEGRAM_BOT_TOKEN = ''; // bot telegram token
 
 export const REDIRECT_BOT = "None" // a redirect bot when the bot is full
 
-export const OWNER_ID = "7879830646" // owner id
+export const OWNER_ID = "" // owner id
 
 export const LIMIT = 30; //number of max sessions for the tele bot
 
-export const MODE = "Nzg3OTgzMDY0NnwxNzU4MjI4MDMxMTkw";
+export const MODE = "Default";
 
 
 
